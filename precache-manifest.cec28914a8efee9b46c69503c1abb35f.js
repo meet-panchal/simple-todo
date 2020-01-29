10e1e021f8be5191f0ac2905f8a988462678d985
Meet Panchal <connect.meetpanchal@gmail.com>
2020-01-29 21:07:22 +0530

    Updates

---
+++ precache-manifest.cec28914a8efee9b46c69503c1abb35f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4b2972dbcd606910de47016e6f3b5d7",
+    "url": "/react-simpleTodo/index.html"
+  },
+  {
+    "revision": "3ed8a762c6fd7c23b37a",
+    "url": "/react-simpleTodo/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "eb0c45ac59d323ddf992",
+    "url": "/react-simpleTodo/static/css/main.7581440b.chunk.css"
+  },
+  {
+    "revision": "3ed8a762c6fd7c23b37a",
+    "url": "/react-simpleTodo/static/js/2.2964545d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-simpleTodo/static/js/2.2964545d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eb0c45ac59d323ddf992",
+    "url": "/react-simpleTodo/static/js/main.f8a04f27.chunk.js"
+  },
+  {
+    "revision": "cf609f60023968692ab6",
+    "url": "/react-simpleTodo/static/js/runtime-main.95784636.js"
+  }
+]);
